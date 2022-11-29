56a03f51f253f72a25f03a69dc5138e953b5e86b
Rafael de Luca <deluca1712@gmail.com>
2022-11-29 12:48:58 -0300

    Tipos de dados

---
+++ tipos_dados/script.js
@@ -0,0 +1,47 @@
+const a = 17;
+const b = 17.12;
+
+const c = "Rafael de Luca"
+const d = 'r'
+
+const e = true;
+const f = false;
+
+const g = {name: 'Rafael', age: '29'}
+const h = ['r',true,3.1415, 'Joao da Silva']
+
+const i = null;
+const j = undefined;
+
+
+console.log("Imprimindo constantes");
+
+console.log(a);
+console.log(typeof a);
+
+console.log(b);
+console.log(typeof b);
+
+console.log(c);
+console.log(typeof c);
+
+console.log(d);
+console.log(typeof d);
+
+console.log(e);
+console.log(typeof e);
+
+console.log(f);
+console.log(typeof f);
+
+console.log(g);
+console.log(typeof g);
+
+console.log(h);
+console.log(typeof h);
+
+console.log(i);
+console.log(typeof i);
+
+console.log(j);
+console.log(typeof j);
