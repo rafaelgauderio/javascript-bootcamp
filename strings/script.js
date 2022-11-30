719badcefbdfbc9504dc9c40aff891614262dbde
Rafael de Luca <deluca1712@gmail.com>
2022-11-30 16:09:03 -0300

    Principais funcoes com string

--- strings/script.js
+++ strings/script.js
@@ -32,3 +32,37 @@ console.log(`Valor da variável  na base HEXADECIMAL é: ${valor4} e o tipo é: 
 
 const euller = parseFloat("2.718281");
 console.log("\nNúmero de euller: " + euller.toFixed(4))
+
+//funcoes com strings
+
+console.log("\n\n")
+const string3 = "Programador Rafael de Luca      ";
+console.log(`"Programador Rafael de Luca      "`);
+
+console.log (`Minúscula: ${string3.toLowerCase}`);
+console.log (`Maiúscula: ${string3.toUpperCase}`);
+// arrays começam no valor 0
+console.log (`Valor do caracter na posição 2: ${string3.charAt(1)}`);
+console.log (`Valor do caracter na posição 6: ${string3.charAt(5)}`);
+console.log (`Valor do caracter na posição 10: ${string3[9]}`);
+
+console.log(`replace: ${string3.replace("Rafael","João")}`);
+console.log(`reclace all: ${string3.replace(/a/g,"e")}`);
+
+console.log(`indexOf:  ${string3.indexOf("a")}`);
+console.log(`lastIndexOf:  ${string3.lastIndexOf("a")}`);
+console.log(`lengh:  ${string3.length}`);
+
+console.log("\nRecortando um pedaço de uma String");
+console.log(`substring: ${string3.substring(0,11)}`)
+
+console.log("Pegando o dia de uma data");
+let data = "17/12/1992";
+
+const dia = Number(data.substring(0,2));
+console.log(dia);
+
+// apagando os espaços em brancos
+console.log(`trim: ${string3.trim()}`);
+
+
