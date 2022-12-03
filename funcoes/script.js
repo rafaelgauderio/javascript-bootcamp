5a024a6c6b988c8d22693917ebf72f0581d03c8c
Rafael De Luca <deluca1712@gmail.com>
2022-12-03 14:26:43 -0300

    Funcoes como argumento de funcoes

--- funcoes/script.js
+++ funcoes/script.js
@@ -3,7 +3,7 @@ console.log("\nQuato maneiras de declarar uma função ");
 function produto(valor1, valor2) {
   return valor1 * valor2;
 }
-console.log("Produto"+ produto(2,15));
+console.log("Produto: "+ produto(2,15));
 
 
 const soma = function (valor1,valor2) {
@@ -35,5 +35,22 @@ function areaRetangulo(a,b) {
 console.log("Área do retangulo:" + areaRetangulo(8,5));
 //console.log(area);
 
+console.log("\nFunction hoinsting é um caracteríticas do interpretador javaScript que move as decarações de funções, variáveis e classes para o topo do escopo do algoritmo.");
+imprimirNome("Rafael de Luca");
+function imprimirNome(nome) {
+    console.log("Seu nome é " + nome);
+}
 
+console.log("\nFunções para ser passadas como agumento de funções e até ser retornadas em funções")
+function quadrado(valor) {
+  return valor * valor;
+}
+
+function dobroDoQuadrado(funcao, valor) {
+  let resultado = funcao(valor) * 2;
+  console.log("O dobro do quadrado de " + valor + " é igual " + resultado);
+}
+
+dobroDoQuadrado(quadrado,3);
+dobroDoQuadrado(quadrado,5);
 
