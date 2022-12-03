3cebd74eabfd1f7ac113ad7d8f96a7fdc889542d
Rafael De Luca <deluca1712@gmail.com>
2022-12-03 16:13:03 -0300

    funcao de alta ordem - filter

--- funcoes_alta_ordem/script.js
+++ funcoes_alta_ordem/script.js
@@ -24,3 +24,34 @@ console.log(map1);
 console.log(map2);
 console.log(map3);
 console.log(map4);
+
+console.log("\nFilter é uma função que retorna uma nova coleção apenas com os elementos que atenderem ao predicado.")
+console.log("\nPredicado: é uma função que retorna true ou false")
+
+function par (valor) {
+  if(valor % 2 ==0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function impar(valor) {
+  if(valor % 2 !=0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+let filter1 = vetor1.filter(par);
+let filter2 = vetor1.filter(impar);
+const filter3 = vetor1.filter(x => x %2==0);
+const filter4 = vetor1.filter(x => x %2!=0);
+var filter5 = vetor1.filter(x => x < 0);
+
+console.log(filter1);
+console.log(filter2);
+console.log(filter3);
+console.log(filter4);
+console.log(filter5);
